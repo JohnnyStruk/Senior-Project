76c80a2e89cf390290bd7ffa405e0bf3e35f78f8
JohnnyStruk <jjstruckman3@gmail.com>
2025-11-05 10:04:21 -0500

    WIP firmware for numpad (not compilable yet)

---
+++ untitled_super_keyboard_numpad/keyboard.c
@@ -0,0 +1,14 @@
+#include "quantum.h"
+#include "i2c_master.h"
+#include "mcp23017.h"
+
+void keyboard_pre_init_kb(void) {
+    i2c_init();
+    mcp23017_init();
+
+    keyboard_pre_init_user();
+}
+
+void keyboard_post_init_kb(void) {
+    keyboard_post_init_user();
+}
