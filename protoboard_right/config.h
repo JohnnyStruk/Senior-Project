6b41c808e82f7c083f58c149bdb16ae2bc0dea8e
JohnnyStruk <jjstruckman3@gmail.com>
2025-11-12 16:25:03 -0500

    stripped down protoboard firmwares to better represent protoboard

--- protoboard_right/config.h
+++ protoboard_right/config.h
@@ -1,26 +1,11 @@
 #pragma once
 
-/* RP2040 I2C setup */
-#define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN GP4
-#define I2C1_SCL_PIN GP5
-#define I2C1_CLOCK_SPEED 400000
-
 /* Matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 5
 
-/* Prevent issues with interrupt pins */
-#define MCP23017_INTB_PIN   NO_PIN
-#define MCP23017_INTA_PIN   NO_PIN
-#define MCP23017_RESET_PIN  NO_PIN
-
-/* MCP23017 addresses */
-#define MCP23017_ADDRESSES {0x25}
-
-#define MCP23017_ROW_PINS { B0, B1, B2, B3 }
-#define MCP23017_COL_PINS { A0, A1, A2, A3, A4 }
+#define MATRIX_ROW_PINS { GP2, GP3, GP4, GP5 }
+#define MATRIX_COL_PINS { GP6, GP7, GP8, GP9, GP10 }
 
 /* Tuning */
-#define DEBOUNCE 10
-#define MATRIX_IO_DELAY 30
+#define DEBOUNCE 5
